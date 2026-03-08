d60f69d51b7c52c8124ade85d399f7f30a8df656
Don Dew <152563752+DewmikaSenarathna@users.noreply.github.com>
2026-03-08 21:45:52 +0530

    feat: add decision tree ml model

---
+++ ml/model/ML_Model/Desition_Tree/Decision_Tree_Model.c
@@ -0,0 +1,94 @@
+#include <string.h>
+void score(double * input, double * output) {
+    double var0[3];
+    if (input[4] <= 65.66500091552734) {
+        if (input[3] <= 28.630000114440918) {
+            memcpy(var0, (double[]){0.05263157894736842, 0.9473684210526315, 0.0}, 3 * sizeof(double));
+        } else {
+            if (input[4] <= 64.01499938964844) {
+                if (input[4] <= 62.8700008392334) {
+                    if (input[4] <= 61.77499961853027) {
+                        memcpy(var0, (double[]){1.0, 0.0, 0.0}, 3 * sizeof(double));
+                    } else {
+                        memcpy(var0, (double[]){0.9827586206896551, 0.017241379310344827, 0.0}, 3 * sizeof(double));
+                    }
+                } else {
+                    if (input[6] <= 43.5) {
+                        memcpy(var0, (double[]){0.8125, 0.1875, 0.0}, 3 * sizeof(double));
+                    } else {
+                        memcpy(var0, (double[]){0.9861111111111112, 0.013888888888888888, 0.0}, 3 * sizeof(double));
+                    }
+                }
+            } else {
+                if (input[6] <= 41.5) {
+                    if (input[3] <= 30.630000114440918) {
+                        memcpy(var0, (double[]){0.1, 0.9, 0.0}, 3 * sizeof(double));
+                    } else {
+                        memcpy(var0, (double[]){0.6, 0.4, 0.0}, 3 * sizeof(double));
+                    }
+                } else {
+                    if (input[6] <= 44.5) {
+                        memcpy(var0, (double[]){0.9, 0.1, 0.0}, 3 * sizeof(double));
+                    } else {
+                        memcpy(var0, (double[]){1.0, 0.0, 0.0}, 3 * sizeof(double));
+                    }
+                }
+            }
+        }
+    } else {
+        if (input[5] <= 122.18999862670898) {
+            if (input[3] <= 25.074999809265137) {
+                if (input[4] <= 81.25) {
+                    memcpy(var0, (double[]){0.0, 0.5384615384615384, 0.46153846153846156}, 3 * sizeof(double));
+                } else {
+                    memcpy(var0, (double[]){0.0, 0.0, 1.0}, 3 * sizeof(double));
+                }
+            } else {
+                if (input[3] <= 33.095001220703125) {
+                    if (input[6] <= 47.5) {
+                        memcpy(var0, (double[]){0.00644122383252818, 0.9780412823891085, 0.015517493778363343}, 3 * sizeof(double));
+                    } else {
+                        memcpy(var0, (double[]){0.8222222222222222, 0.17777777777777778, 0.0}, 3 * sizeof(double));
+                    }
+                } else {
+                    if (input[3] <= 33.579999923706055) {
+                        memcpy(var0, (double[]){0.6, 0.4, 0.0}, 3 * sizeof(double));
+                    } else {
+                        memcpy(var0, (double[]){1.0, 0.0, 0.0}, 3 * sizeof(double));
+                    }
+                }
+            }
+        } else {
+            if (input[6] <= 34.5) {
+                if (input[3] <= 29.389966011047363) {
+                    if (input[6] <= 33.5) {
+                        memcpy(var0, (double[]){0.00014407145944388418, 0.005330643999423714, 0.9945252845411324}, 3 * sizeof(double));
+                    } else {
+                        memcpy(var0, (double[]){0.0, 0.2916666666666667, 0.7083333333333334}, 3 * sizeof(double));
+                    }
+                } else {
+                    if (input[4] <= 72.14054107666016) {
+                        memcpy(var0, (double[]){0.75, 0.25, 0.0}, 3 * sizeof(double));
+                    } else {
+                        memcpy(var0, (double[]){0.0, 0.9629629629629629, 0.037037037037037035}, 3 * sizeof(double));
+                    }
+                }
+            } else {
+                if (input[4] <= 71.79553985595703) {
+                    if (input[4] <= 71.73054122924805) {
+                        memcpy(var0, (double[]){0.12, 0.88, 0.0}, 3 * sizeof(double));
+                    } else {
+                        memcpy(var0, (double[]){0.93125, 0.0625, 0.00625}, 3 * sizeof(double));
+                    }
+                } else {
+                    if (input[3] <= 25.755000114440918) {
+                        memcpy(var0, (double[]){0.0, 0.07142857142857142, 0.9285714285714286}, 3 * sizeof(double));
+                    } else {
+                        memcpy(var0, (double[]){0.0, 0.93993993993994, 0.06006006006006006}, 3 * sizeof(double));
+                    }
+                }
+            }
+        }
+    }
+    memcpy(output, var0, 3 * sizeof(double));
+}
