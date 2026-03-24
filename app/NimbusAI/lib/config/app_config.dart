09bf9bd04886fc2b659dafe38613189cbe834f1c
Don Dew <152563752+DewmikaSenarathna@users.noreply.github.com>
2026-03-24 22:22:47 +0530

    Refactor: Updated file structure

--- app/NimbusAI/lib/config/app_config.dart
+++ app/NimbusAI/lib/config/app_config.dart
@@ -3,12 +3,11 @@ class AppConfig {
 
   static const String firebaseDatabaseUrl = String.fromEnvironment(
     'FIREBASE_DATABASE_URL',
-    defaultValue:
-        'https://rainfallprediction-945cd-default-rtdb.asia-southeast1.firebasedatabase.app',
+    defaultValue: '',
   );
 
   static const String sensorApiEndpoints = String.fromEnvironment(
     'SENSOR_API_ENDPOINTS',
-    defaultValue: 'http://10.19.40.192/data,http://192.168.4.1/data',
+    defaultValue: '',
   );
 }
